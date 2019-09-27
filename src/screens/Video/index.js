d0939c7edfc50de36138b813912aea1bb88da154
Benjamin_Pcrd <benjamin.picard0@gmail.com>
2019-09-27 18:36:24 +0100

    webView

--- src/screens/Video/index.js
+++ src/screens/Video/index.js
@@ -1,8 +1,11 @@
 import React, { Component } from "react";
 import {
   View,
-  Text
+  Text,
+  StyleSheet
 } from 'react-native';
+
+import WebView from 'react-native-webview';
 
 export default class Video extends Component {
   static navigationOptions = {
@@ -12,14 +15,45 @@ export default class Video extends Component {
   render() {
     
     return (
-      <View>
-        <Text>id: {this.props.navigation.getParam('item').id}</Text>
-        <Text>title: {this.props.navigation.getParam('item').title}</Text>
-        <Text>subtitle: {this.props.navigation.getParam('item').subtitle}</Text>
-        <Text>description: {this.props.navigation.getParam('item').description}</Text>
-        <Text>image: {this.props.navigation.getParam('item').image}</Text>
-        <Text>url: {this.props.navigation.getParam('item').url}</Text>
+      <View style={styles.container}>
+        <View style={styles.titleView}>
+          <Text style={styles.title}>{this.props.navigation.getParam('item').title}</Text>
+          <Text>{this.props.navigation.getParam('item').subtitle}</Text>
+        </View>
+        <WebView
+          source={{html: '<iframe width="1280" height="720" src="' + this.props.navigation.getParam('item').url + '" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'}}
+          style={styles.webView}
+        />
+        <View style={styles.descriptionView}>
+          <Text>
+          {this.props.navigation.getParam('item').description}</Text>
+        </View>
       </View>
+      
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  titleView: {
+    flex: 0,
+    margin: 10,
+    alignItems: 'center'
+  },
+  webView: {
+    flex: 1,
+    margin: 10
+  },
+  descriptionView: {
+    flex: 1,
+    margin: 10
+  },
+  title: {
+    fontSize: 30,
+    fontWeight: 'bold'
+  }
+});
+
