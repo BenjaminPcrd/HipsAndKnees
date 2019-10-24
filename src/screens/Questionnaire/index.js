096c6cdc2a11035804000df51b7fa9824db5b534
Benjamin_Pcrd <benjamin.picard0@gmail.com>
2019-10-24 18:50:57 +0100

    answers saved to the db, and questionnaire check

--- src/screens/Questionnaire/index.js
+++ src/screens/Questionnaire/index.js
@@ -1,8 +1,10 @@
 import React, { Component } from "react";
 import {
   FlatList,
-  Button
+  Button,
+  Alert
 } from 'react-native';
+import { ToastAndroid } from 'react-native';
 
 import { GoogleSignin } from 'react-native-google-signin';
 import firebase from 'react-native-firebase';
@@ -28,23 +30,34 @@ export default class Questionnaire extends Component {
   }
 
   _submit() {
-    const ref = firebase.firestore().collection('users').doc(this.state.user.user.id);
-    firebase.firestore().runTransaction(async transaction => {
-      const doc = await transaction.get(ref);
-      
-      if(!doc.exists) {
-        transaction.set(ref, {id: this.state.user.user.id})
-        console.log("doc not exists")
-      } else {
-        transaction.update(ref, {id: this.state.user.user.id})
-        console.log("doc exists")
-      }
+    if(!this._answers.find(i => i.answers.length == 0)) {
+      const ref = firebase.firestore().collection('users').doc(this.state.user.user.id);
+      firebase.firestore().runTransaction(async transaction => {
+        const doc = await transaction.get(ref);
+        
+        if(!doc.exists) {
+          transaction.set(ref, {user: this.state.user.user, questionnaire: [{date: new Date(), results: this._answers}]})
+        } else {
+          var prevQuest = doc._data.questionnaire
+          transaction.update(ref, {user: this.state.user.user, questionnaire: [...prevQuest, {date: new Date(), results: this._answers}]})
+        }
 
-    }).then(() => console.log("updated")).catch(err => console.log("error: " + err))
+      }).then(() => {
+        Alert.alert('Thank you', 'Answers have been saved', [
+          {text: 'ok', onPress: () => this.props.navigation.goBack()}
+        ])
+      }).catch(err => {
+        console.log(err)
+        Alert.alert('Oups, An error occurred', err+"", [
+          {text: 'ok', onPress: () => this.props.navigation.goBack()}
+        ])
+      })
+    } else {
+      ToastAndroid.show('Please, answer all questions', ToastAndroid.SHORT);
+    }
   }
 
   _addAnswer(answer, question) {
-    //console.log(typeof(answer))
     var element = this._answers.find(i => i.question.id == question.id)
     if(question.type == "MULTIPLE_CHOICE_MULTIPLE_ANSWER") {
       if(element.answers.includes(answer)) {
@@ -59,7 +72,6 @@ export default class Questionnaire extends Component {
         this._answers.find(i => i.question.id == question.id).answers[0] = answer
       }
     }
-    //console.log(this._answers.find(i => i.question.id == question.id).answers)
   }
   
   render() {
