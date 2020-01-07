8ae9adb7a734e73cafa2fd4a89478b361caeacc7
BenjaminPcrd <benjamin.picard0@gmail.com>
2020-01-07 17:01:12 +0100

    loading the videos details from firestore instead the json file

--- src/screens/ExerciseVideos/index.js
+++ src/screens/ExerciseVideos/index.js
@@ -4,17 +4,28 @@ import {
 } from 'react-native';
 import TypeCard from '../../components/TypeCard';
 
-const data = require('./data.json')
+import firebase from 'react-native-firebase';
 
 export default class ExerciseVideos extends Component {
   static navigationOptions = {
     title: 'Exercise Videos',
   };
+
+  state = {
+    data: ""
+  }
+
+  async componentDidMount(props) {
+    const ref = firebase.firestore().collection('videos')
+    var videos = await ref.get()
+    videos = videos._docs.map(x => x._data)
+    this.setState({data: videos})
+  }
   
   render() {
     return (
       <FlatList
-        data={data}
+        data={this.state.data}
         renderItem={({ item }) => <TypeCard item={item} onPress={() => this.props.navigation.navigate('VideoList', { item: item })}/>}
         keyExtractor={item => item.id}
       />
